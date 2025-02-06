23d4adcd91393e999ebd4cf67b23e5880817b918
theCodingMan-1 <brynmorpeters@gmail.com>
2025-02-06 22:13:25 +0000

    just some sneaky revision

--- Lectures/Week3.java
+++ Lectures/Week3.java
@@ -18,6 +18,28 @@ public class Runner {
 
         mDolphin.makeNoise(); // "Squeak Click"
         mDog.makeNoise(); // "Woof Woof"
+
+
+        mLion.acceptMammal(mDog);
+        mDog.acceptMamal(mLion);
     }
+}
+
+
+public abstract class Mammal {
+    public abstract void makeNoise();
+    public abstract void acceptMamal(Mammal m);
+    public abstract makeNoise(Dog d);
+    public abstract makeNoise(Lion l);
+    public abstract makeNoise(Dolphin d);
+
+}
+
+
+public class Dog extends Mammal {
+    public void makeNoise() {System.out.println("Woof")}
+
+    public void acceptMamal(Mammal m) {m.makeNoise(this);}
+    // calls the make noise method of the underlying object on the heap
 }
 
